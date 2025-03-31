bbc4241c32dc5596fe6f7c0a7a896d57d31b6a24
hossein <h.ebr141@outlook.com>
2025-04-01 00:10:26 +0330

    Remove the local chain from production app

--- web3/wagmiConfig.ts
+++ web3/wagmiConfig.ts
@@ -1,34 +1,36 @@
-import { defineChain, type AppKitNetwork } from "@reown/appkit/networks"
+import { defineChain, type AppKitNetwork, type Chain } from "@reown/appkit/networks"
 import { http, createConfig } from "@wagmi/vue"
 import { sepolia } from "@wagmi/vue/chains"
 
-const hardhatLocalhost = defineChain({
-  id: 31_337,
-  chainNamespace: "eip155",
-  caipNetworkId: "eip155:31337",
-  name: "Localhost",
-  nativeCurrency: {
-    decimals: 18,
-    name: "Ether",
-    symbol: "ETH",
-  },
-  rpcUrls: {
-    default: { http: ["http://127.0.0.1:8545"] },
-  },
-  testnet: true,
-})
+let hardhatLocalhost: Chain | null = null
+if (import.meta.dev) {
+  hardhatLocalhost = defineChain({
+    id: 31_337,
+    chainNamespace: "eip155",
+    caipNetworkId: "eip155:31337",
+    name: "Localhost",
+    nativeCurrency: {
+      decimals: 18,
+      name: "Ether",
+      symbol: "ETH",
+    },
+    rpcUrls: {
+      default: { http: ["http://127.0.0.1:8545"] },
+    },
+    testnet: true,
+  })
+}
 
+const chains: [Chain, ...Chain[]] = hardhatLocalhost ? [hardhatLocalhost, sepolia] : [sepolia]
+const transports = hardhatLocalhost ? { [sepolia.id]: http(), [hardhatLocalhost.id]: http() } : { [sepolia.id]: http() }
 const config = createConfig({
-  chains: [sepolia, hardhatLocalhost],
-  transports: {
-    [sepolia.id]: http(),
-    [hardhatLocalhost.id]: http(),
-  },
+  chains,
+  transports,
 })
 
-const networks: [AppKitNetwork, ...AppKitNetwork[]] = [hardhatLocalhost, sepolia]
+const networks: [AppKitNetwork, ...AppKitNetwork[]] = chains
 
-export { hardhatLocalhost, config, networks }
+export { config, networks }
 
 declare module "@wagmi/vue" {
   interface Register {
